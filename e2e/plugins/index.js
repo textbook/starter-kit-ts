8745b2491d04c872e9d0e068e574a64161b4fe51
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-05-21 22:38:19 +0100

    Remove explicit Cypress TS preprocessor
    This is unneeded since 4.4.0:
    
    https://docs.cypress.io/guides/references/changelog.html#4-4-0

--- e2e/plugins/index.js
+++ e2e/plugins/index.js
@@ -1,21 +1,8 @@
-const wp = require("@cypress/webpack-preprocessor");
+/// <reference types="cypress" />
 
-module.exports = (on) => {
-	const options = {
-		webpackOptions: {
-			resolve: {
-				extensions: [".ts", ".tsx", ".js"],
-			},
-			module: {
-				rules: [
-					{
-						test: /\.tsx?$/,
-						loader: "ts-loader",
-						options: { transpileOnly: true },
-					},
-				],
-			},
-		},
-	};
-	on("file:preprocessor", wp(options));
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+
 };
